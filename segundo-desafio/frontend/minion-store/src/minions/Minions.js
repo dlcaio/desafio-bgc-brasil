0f3316694aab281e9a5dc2552ba80db48ad29550
Caio Della Libera <caiodellalibera@id.uff.br>
2020-08-09 20:19:38 -0300

    adding minions component

--- segundo-desafio/frontend/minion-store/src/minions/Minions.js
+++ segundo-desafio/frontend/minion-store/src/minions/Minions.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
 
 export default class Minions extends Component {
+    constructor(props) {
+        super()
+        this.state = {
+            name: ''
+            
+        }
+    }
     render() {
         return (
             <div>
-                
+                Minions
             </div>
         )
     }
