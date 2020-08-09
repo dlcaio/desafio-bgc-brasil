033c86f24fc5885fec0fe5cf7e1a61eb3448c293
Caio Della Libera <caiodellalibera@id.uff.br>
2020-08-09 20:21:18 -0300

    adding shopping cart component

--- segundo-desafio/frontend/minion-store/src/App.js
+++ segundo-desafio/frontend/minion-store/src/App.js
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 import Minions from './minions/Minions'
+import ShoppingCart from './shopping-cart/ShoppingCart';
 
 function App() {
   return (
     <div className="App">
       <Minions/>
+      <ShoppingCart/>
     </div>
   );
 }
