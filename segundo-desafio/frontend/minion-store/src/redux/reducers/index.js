d4eafb8fae26796579c0bb1753d0bae6a1ebd6c6
Caio Della Libera <caiodellalibera@id.uff.br>
2020-08-09 21:13:22 -0300

    adding minions to redux initial state

--- segundo-desafio/frontend/minion-store/src/redux/reducers/index.js
+++ segundo-desafio/frontend/minion-store/src/redux/reducers/index.js
@@ -1,15 +1,18 @@
 const initialState = {
-    minions: []
+    minions: [
+      {id: 1, name: 'Arnoldo', price: 300, onChart: false},
+      {id: 2, name: 'Arnilson', price: 150, onChart: false},
+      {id: 3, name: 'Arnan', price: 30, onChart: false},]
   };
 
 
 function rootReducer(state = initialState, action) {
 
   if (action.type === 'REMOVE_MINION') {
-    console.log(state.articles)
+    console.log(state.minions)
     console.log(action.id)
     return {minions: state.minions.filter(minion => 
-      minion.id !== minion.id)}
+      minion.id !== action.id)}
   }
   return state;
 };
